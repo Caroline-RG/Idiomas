8ed166f4fdeda74a038a30961d39b77ff7cad8e6
Caroline Ranilla Gonzales <71720441+Caroline-RG@users.noreply.github.com>
2021-05-06 18:13:57 -0500

    Actualización 4

---
+++ Idiomas/script.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.dia(nombre) VALUES ('Lunes');
+INSERT INTO public.dia(nombre) VALUES ('Martes');
+INSERT INTO public.dia(nombre) VALUES ('Miércoles');
+INSERT INTO public.dia(nombre) VALUES ('Jueves');
+INSERT INTO public.dia(nombre) VALUES ('Viernes');
+INSERT INTO public.dia(nombre) VALUES ('Sábado');
+INSERT INTO public.dia(nombre) VALUES ('Domingo');
